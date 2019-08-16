5be4528bf77c1dee16e7687ee8f2761291281129
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-08-15 23:11:53 -0500

    put added

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,6 @@
 class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
+
   def create
     image = Image.new(image_params)
 
@@ -19,7 +21,24 @@ class ImagesController < ApplicationController
     render json: images, status: :ok
   end
 
+  def update
+    respond_to do |format|
+      if @image.update(image_params)
+        ActionCable.server.broadcast(
+        'room_image_channel', content: {message: "hello"}
+      )
+        format.json { render :show, status: :ok, location: @image }
+      else
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
   private
+  def set_image
+    @image = Image.find(params[:id])
+  end
   def image_params
     params.require(:image).permit(:state, :image, :name)
   end
