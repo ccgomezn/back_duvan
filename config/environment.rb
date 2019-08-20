acc2f5c3c8475c5f3b3aa2120b7b3c0a76a84149
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-08-20 00:51:26 -0500

    env full

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.routes.default_url_options[:host] = '<%= ENV["host"] %>'
+Rails.application.routes.default_url_options[:host] = ENV["host"]
 
 Rails.application.initialize!
