3afb4667aac3ec25b30eb95726536c3cc21d1718
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-08-20 00:36:04 -0500

    r blob

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -2,6 +2,6 @@ class ImageSerializer < ActiveModel::Serializer
     attributes :id, :name, :state, :created_at, :updated_at, :image_url
     
     def image_url
-        self.object.image.service_url
+        ActiveStorage::rails_blob_url(self.object.image)
     end
 end
