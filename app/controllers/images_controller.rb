94422cf14e7a0015054879c02a25a1e93469e74b
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-08-22 00:48:39 -0500

    not priority

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -36,8 +36,8 @@ class ImagesController < ApplicationController
 
   def fcm_call_notification
     fcm = FCM.new(ENV['push_id'], :timeout => 30)
-    options = {notification: {title: "Nuevo pedido!", body: "Hola, tienes un nuevo pedido en la bodega!"},
-               priority: "high"}
+    options = {notification: {title: "Nuevo pedido!", body: "Hola, tienes un nuevo pedido en la bodega!", priority: "high", collapse_key:"updated_score"},
+               priority: "high", collapse_key:"updated_score"}
     response = fcm.send_to_topic('global',
                                  options)
   end
